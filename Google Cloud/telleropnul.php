569d83abd48091bcb2443cf4183640f558b48f99
bazzz1512 <sebastianspeekenbrink@gmail.com>
2018-11-29 12:47:34 +0100

    Stupid me
    Uploaded the wrong files, lol

--- Google Cloud/telleropnul.php
+++
@@ -1,5 +0,0 @@
-﻿<?php 
-session_start();
- $_SESSION['teller'] = 0;
-header ('Location: inlog.php');	
-?>
--- Google Cloud/uitloggen.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-session_start();
-$_SESSION = array();
-session_destroy();
-header ('Location: inlog.php');	
-?>
